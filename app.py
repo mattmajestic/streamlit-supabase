5f03011e6f7bc6d8a84fe5a976c787246de5a20b
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-08-03 22:18:26 -0400

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import requests
 import os
 import base64
+import requests
 import supabase
 
 # Fetch Supabase URL and key from environment variables
@@ -9,37 +9,38 @@ supabase_url = os.environ.get('SUPABASE_URL')
 supabase_key = os.environ.get('SUPABASE_KEY')
 
 # Initialize Supabase client
-supabase_client = supabase.Client(supabase_url, supabase_key)
+supabase_client = supabase.create_client(supabase_url, supabase_key)
 
 def main():
     st.title('Supabase File Upload')
 
-    # Get the file URL from user
-    file_url = st.text_input('Enter file URL:')
-    if file_url:
-        st.write(f'File URL: {file_url}')
+    # Get the file URLs or file uploads from the user
+    uploaded_files = st.file_uploader("Choose CSV files", accept_multiple_files=True)
 
-        # Fetch file content from the URL
-        response = requests.get(file_url)
-        file_content = response.content
+    if uploaded_files:
+        for file in uploaded_files:
+            st.write(f'File name: {file.name}')
 
-        # Get the file name and extension from the URL
-        file_name = file_url.split('/')[-1]
-        file_ext = file_name.split('.')[-1]
+            # Read file content
+            file_content = file.read()
 
-        # Encode file content in base64
-        file_base64 = base64.b64encode(file_content).decode()
+            # Get the file name and extension
+            file_name = file.name
+            file_ext = file_name.split('.')[-1]
 
-        # Upload file to Supabase
-        file_metadata = {
-            'name': file_name,
-            'type': f'image/{file_ext}'
-        }
-        supabase_response = supabase_client.storage().upload(file_name, file_base64, file_metadata)
-        if supabase_response['error']:
-            st.error('File upload failed.')
-        else:
-            st.success('File uploaded successfully.')
+            # Encode file content in base64
+            file_base64 = base64.b64encode(file_content).decode()
+
+            # Upload file to Supabase
+            file_metadata = {
+                'name': file_name,
+                'type': f'application/csv'  # You can change the content type based on the file type you are uploading
+            }
+            supabase_response = supabase_client.storage().upload(file_name, file_base64, file_metadata)
+            if supabase_response['error']:
+                st.error(f'File upload failed for {file.name}.')
+            else:
+                st.success(f'File {file.name} uploaded successfully.')
 
 if __name__ == '__main__':
     main()
