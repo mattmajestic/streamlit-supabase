2f499b4f46c8d2661f36d525a9f2ba40fcf43d6d
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-08-03 22:15:50 -0400

    update

--- app.py
+++ app.py
@@ -12,55 +12,34 @@ supabase_key = os.environ.get('SUPABASE_KEY')
 supabase_client = supabase.Client(supabase_url, supabase_key)
 
 def main():
-    st.title("Supabase Auth and Bucket Storage Demo")
+    st.title('Supabase File Upload')
 
-    st.header("Login")
+    # Get the file URL from user
+    file_url = st.text_input('Enter file URL:')
+    if file_url:
+        st.write(f'File URL: {file_url}')
 
-    email_login = st.text_input("Email", key="email_login")
-    password_login = st.text_input("Password", type="password", key="password_login")
-    login_button = st.button("Login")
+        # Fetch file content from the URL
+        response = requests.get(file_url)
+        file_content = response.content
 
-    if login_button:
-        # Perform login logic here
-        st.write(f"Logging in with email: {email_login}")
+        # Get the file name and extension from the URL
+        file_name = file_url.split('/')[-1]
+        file_ext = file_name.split('.')[-1]
 
-    st.header("Register")
+        # Encode file content in base64
+        file_base64 = base64.b64encode(file_content).decode()
 
-    email_register = st.text_input("Email", key="email_register")
-    password_register = st.text_input("Password", type="password", key="password_register")
-    register_button = st.button("Register")
-
-    if register_button:
-        # Perform register logic here
-        st.write(f"Registering with email: {email_register}")
-
-    st.header("Bucket Storage")
-
-    file_upload = st.file_uploader("Choose a file to upload")
-
-    if file_upload:
-        # Encode the file to base64
-        file_data = file_upload.read()
-        file_data_encoded = base64.b64encode(file_data).decode()
-
-        # Upload the file to Supabase bucket
-        file_name = os.path.basename(file_upload.name)
-        file_extension = os.path.splitext(file_name)[1]
-        file_url = f"{supabase_url}/storage/v1/object/public/{file_name}"
+        # Upload file to Supabase
         file_metadata = {
-            "name": file_name,
-            "file_extension": file_extension,
-            "data": file_data_encoded
+            'name': file_name,
+            'type': f'image/{file_ext}'
         }
-        supabase_response = supabase_client.storage.from_url(file_url).upload(file_metadata)
-
-        # Display the download URL
-        if supabase_response["data"]:
-            download_url = supabase_response["data"]["url"]
-            st.success("File uploaded successfully!")
-            st.markdown(f"**Download URL:** [Download]({download_url})")
+        supabase_response = supabase_client.storage().upload(file_name, file_base64, file_metadata)
+        if supabase_response['error']:
+            st.error('File upload failed.')
         else:
-            st.error("Error uploading file.")
+            st.success('File uploaded successfully.')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
