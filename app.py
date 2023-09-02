3ffd048b95644774df4b0142aeb7f331ebca232f
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-09-01 20:08:42 -0400

    test 3

--- app.py
+++ app.py
@@ -19,17 +19,17 @@ def add_database_record():
     
     # Create the data dictionary with 'id' and 'created_at' timestamp
     formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
-    data = [{'id': unique_id, 'created_at': formatted_date}]
+    data = {'id': unique_id, 'created_at': formatted_date}
     
     # Insert the data into the "streamlit" table
-    response = supabase.table('streamlit').upsert(data, ['id'])
+    response = supabase.table('streamlit').upsert([data], ['id'])
     
     # Check if the insert was successful
     if response.status_code == 200:
         st.success('🚀 Record added successfully!')
     else:
         st.error('❌ Error adding record to the database')
-
+        
 def show_user_info(user):
     with st.expander('User Information'):
         st.success(f'🎉 Logged in as: {user["email"]}')
