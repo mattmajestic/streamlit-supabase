87003774d3736288418628a4a76d1ef567ebd24f
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-09-06 21:40:21 -0400

    update readme

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="streamlit_supabase",
-    version="0.4",
-    description="Supabase functions in the Streamlit framework",
+    version="0.5",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     author="Matt Majestic",
     packages=find_packages(),
     install_requires=[
